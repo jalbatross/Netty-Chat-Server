e195313e9ea16dfc12cf1d049406898435554dce
jalbatross <ih8u2o@gmail.com>
2017-05-19 12:31:17 -0700

    server is now an echo server

--- src/com/test/chatserver/ChatServerHandler.java
+++ src/com/test/chatserver/ChatServerHandler.java
@@ -3,23 +3,35 @@ package com.test.chatserver;
 
 
 import io.netty.buffer.ByteBuf;
-
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 
 /**
  * Handles a server-side chat channel. Only accepts Strings.
  */
-public class ChatServerHandler extends SimpleChannelInboundHandler<String> { // (1)
+public class ChatServerHandler extends ChannelInboundHandlerAdapter { // (1
 	
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, String str) throws Exception {
-		String finalMessage = new String("Name: " + str);
-		ctx.writeAndFlush(finalMessage);
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+	{
+		//receive the message as a bytebuf
+		ByteBuf in = (ByteBuf) msg;
+		
+		//convert it to a string
+		String message = in.toString(CharsetUtil.UTF_8);
+		
+		ChannelFuture cf = ctx.write(Unpooled.copiedBuffer(message,CharsetUtil.UTF_8));
+		ctx.flush();
+		if (!cf.isSuccess()) {
+			System.out.println(cf.cause());
+		}
 		
 	}
-
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
         // Close the connection when an exception is raised.
