f044e0dc1eeb7bdd35033f11ebf7a09f36b19677
joey <ih8u2o@gmail.com>
2017-06-25 14:53:02 -0700

    Finished local Mongo tests

--- src/com/test/chatserver/MongoConnectingTest.java
+++ src/com/test/chatserver/MongoConnectingTest.java
@@ -1,19 +1,19 @@
 package com.test.chatserver;
 
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
 
 import org.bson.Document;
-import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,8 +25,15 @@ public class MongoConnectingTest {
         MongoCollection<Document> collection = database.getCollection("people");
         
         System.out.println(mongoClient.getAddress());
-        return;
+        DBObject query = new BasicDBObject("author", "Dude");
+        MongoIterable<Document> cursor = collection.find(Document.parse(query.toString()));
+        Document jo = cursor.first();
+        System.out.println(jo.get("message"));
+        Gson gson = new Gson();
+        TimeChatMessage lastMsg = new TimeChatMessage("Dude", "Where's my car");
+        String json = gson.toJson(lastMsg);
         
+        //collection.insertOne(Document.parse(json));
         /*
         List<Integer> books = Arrays.asList(27464, 747854);
         DBObject person = new BasicDBObject("_id", "jo")
