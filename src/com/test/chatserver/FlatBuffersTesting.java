079ec37ee68513110d9c94f4b4cf6143684d56ef
joey <ih8u2o@gmail.com>
2017-06-29 16:37:15 -0700

    everything working for schema except lobbies

--- src/com/test/chatserver/FlatBuffersTesting.java
+++ src/com/test/chatserver/FlatBuffersTesting.java
@@ -7,18 +7,31 @@ import com.google.flatbuffers.FlatBufferBuilder;
 import Schema.*;
 public class FlatBuffersTesting {
     
-    public static final int CHAT = 0;
-    public static final int CREDENTIAL = 1;
-    public static final int AUTH = 2;
-    public static final int LOBBIES = 3;
-    
     public static void main (String[] args) {
         FlatBufferBuilder builder = new FlatBufferBuilder(1024);
         
         int user = builder.createString("wahoo");
         int pw = builder.createString("Jones");
         
+        //make credential
         int cred = Credentials.createCredentials(builder, user, pw);
+        
+        //make auth
+        int verif = Auth.createAuth(builder, false);
+        
+        //make chat
+        int chatMsg = Chat.createChat(builder, 6969, 
+                builder.createString("joe"), builder.createString("BLOGGS"));
+        
+        //make lobbies
+        int[] lobbies = new int[4];
+
+        lobbies[0] = builder.createString("Tasma");
+        lobbies[1] = builder.createString("Ceres");
+        lobbies[2] = builder.createString("Larc");
+        lobbies[3] = builder.createString("Gevergreen");
+        
+        int lobbyVec = Lobbies.createListVector(builder, lobbies);
         
         Message.startMessage(builder);
         Message.addDataType(builder, Data.Credentials);
@@ -31,23 +44,79 @@ public class FlatBuffersTesting {
         
         Message decoded = Message.getRootAsMessage(buf);
         
-        switch (decoded.dataType()) {
-            case CHAT:
-                System.out.println("got chat msg");
-                break;
-            case CREDENTIAL:
-                System.out.println("got credentials");
-                break;
-            case AUTH:
-                System.out.println("got auth");
-                break;
-            case LOBBIES:
-                System.out.println("got lobbies");
-                break;
-            default:
-                System.out.println("oh no");
-                break;
+        Message[] messages = new Message[5];
+        messages[0] = decoded;
+        
+        Message.startMessage(builder);
+        Message.addDataType(builder, Data.Auth);
+        Message.addData(builder, verif);
+        int finAuth = Message.endMessage(builder);
+        builder.finish(finAuth);
+        buf = builder.dataBuffer();
+        Message authMsg = Message.getRootAsMessage(buf);
+        messages[1] = authMsg;
+        
+        Message.startMessage(builder);
+        Message.addDataType(builder, Data.Chat);
+        Message.addData(builder, chatMsg);
+        int finChat = Message.endMessage(builder);
+        builder.finish(finChat);
+        buf = builder.dataBuffer();
+        Message chatFin = Message.getRootAsMessage(buf);
+        messages[2] = chatFin;
+        
+        Message.startMessage(builder);
+        Message.addDataType(builder, Data.Lobbies);
+        Message.addData(builder, lobbyVec);
+        int lobbyMsg = Message.endMessage(builder);
+        builder.finish(lobbyMsg);
+        buf = builder.dataBuffer();
+        Message lobbyFin = Message.getRootAsMessage(buf);
+        messages[3] = lobbyFin;
+        
+        messages[4] = authMsg;
+        
+      //expect: cred , auth, chat, lobby, auth"
+        for (int i = 0; i < messages.length; i ++) {
+            switch (messages[i].dataType()) {
+                case Type.Chat:
+                    System.out.println("got chat msg");
+                    Chat chat = (Chat)messages[i].data(new Chat());
+                    System.out.println("author: " + chat.author() + "\n"
+                            + "msg: " + chat.message());
+                    break;
+                case Type.Credentials:
+                    System.out.println("got credentials");
+                    Credentials creds = (Credentials)messages[i].data(new Credentials());
+                    System.out.println("username: " + creds.username() + "\n"
+                            + "pass: " + creds.password());
+                    break;
+                case Type.Auth:
+                    System.out.println("got auth");
+                    Auth auth = (Auth)messages[i].data(new Auth());
+                    if (auth.verified()){
+                        System.out.println("verified");
+                    }
+                    else {
+                        System.out.println("not verified");
+                    }
+                    break;
+                case Type.Lobbies:
+                    System.out.println("got lobbies");
+                    Lobbies lobs = (Lobbies)messages[i].data(new Lobbies());
+                    System.out.println("num of lobbies: " + lobs.listLength());
+                    for (int j = 0; j < lobs.listLength(); j++) {
+                        System.out.println("lobby " + j + ": " + lobs.list(j));
+                    }
+                    break;
+                default:
+                    System.out.println("oh no");
+                    break;
+            }
+            System.out.println("---------");
         }
+        
+
         
     }
 }
