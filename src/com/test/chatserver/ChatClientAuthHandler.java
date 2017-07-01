4923c24b1229dc44fb02e6e94876ab42773b0690
joey <ih8u2o@gmail.com>
2017-06-30 17:28:29 -0700

    ClientAuthHandler written

---
+++ src/com/test/chatserver/ChatClientAuthHandler.java
@@ -0,0 +1,43 @@
+package com.test.chatserver;
+
+import Schema.Auth;
+import Schema.Message;
+import Schema.Type;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.AttributeKey;
+
+/**
+ * Handles FlatBuffers Messages with data type Auth as defined in Schema
+ * 
+ * Flags the "authorized" attribute of the channel to TRUE if the 
+ * Auth has verified as true and false otherwise then forwards the msg
+ * in the pipeline upstream
+ * 
+ * Simply forwards msg upstream if the dataType of the flatBufMsg is not 
+ * Auth.
+ * 
+ * @author jalbatross (Joey Albano)
+ *
+ */
+public class ChatClientAuthHandler extends ChannelInboundHandlerAdapter {
+    
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("[ChatClientAuth] ChannelRead called");
+        ByteBuf buf = (ByteBuf) msg;
+        Message flatbufMsg = Message.getRootAsMessage(buf.nioBuffer());
+        
+        if (flatbufMsg.dataType() == Type.Auth){
+            Auth auth = (Auth) flatbufMsg.data(new Auth());
+            
+            ctx.channel().attr(AttributeKey.valueOf("authorized")).set(auth.verified());
+        }
+        
+        ctx.fireChannelRead(msg);
+        
+        
+    }
+
+}
--- src/com/test/chatserver/ChatClientDecoder.java
+++ src/com/test/chatserver/ChatClientDecoder.java
@@ -1,7 +1,56 @@
 package com.test.chatserver;
 
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class ChatClientDecoder extends ChannelInboundHandlerAdapter {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrameDecoder;
+
+/**
+ *  Decodes ByteBufs by blocking until they are ready to be passed down
+ *  the channel pipeline.
+ *  
+ *  Each segment of data should be prefixed by a 4 byte integer indicating
+ *  its length.
+ * 
+ * @author joey
+ *
+ */
+
+public class ChatClientDecoder extends SimpleChannelInboundHandler<Object> {
+    
+    public static final int MAX_BYTES = 1024;
+    private List<Object> out = new ArrayList<Object>();
+    
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("[ChatClientDecoder] Sending upstream!");
+        
+        if (!(msg instanceof ByteBuf)) {
+            System.out.println("Client decoder received non bytebuf, sending upstream");
+            ctx.fireChannelRead(msg);
+            return;
+        }
+        
+        ByteBuf buf = (ByteBuf) msg;
+        
+        IntegerHeaderFrameDecoder decoder = new IntegerHeaderFrameDecoder();
+        
+        decoder.decode(ctx, buf, out);
+       
+        ctx.fireChannelRead(out.get(0));
+        
+        return;
+        
+    }
+    
 
 }
