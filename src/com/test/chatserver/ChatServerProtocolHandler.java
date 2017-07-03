4731b8ac8e73970e9d2ad224ca2479f6d505345b
joey <ih8u2o@gmail.com>
2017-07-02 23:40:35 -0700

    More protocol handler progress
    Successfully added new handler to pipeline which decodes the HTTP request
    after a browser client sends POST information to the server. Need to find
    out why the protocol is received as OPEN but the request method is POST.

--- src/com/test/chatserver/ChatServerProtocolHandler.java
+++ src/com/test/chatserver/ChatServerProtocolHandler.java
@@ -3,6 +3,10 @@ package com.test.chatserver;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpMessage;
+import io.netty.handler.codec.http.HttpObjectDecoder;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestDecoder;
 
 public class ChatServerProtocolHandler extends ChannelInboundHandlerAdapter {
     private ByteBuf buf;
@@ -32,6 +36,14 @@ public class ChatServerProtocolHandler extends ChannelInboundHandlerAdapter {
         else if (char1 == 'P' && char2 == 'O') {
             System.out.println("[ProtocolHandler] post");
             ctx.close();
+        }
+        else if (char1 == 'O' && char2 == 'P') {
+            System.out.println("[ProtocolHandler] open");
+            System.out.println(ctx.channel().pipeline().names());
+            ctx.channel().pipeline().addAfter("ChatServerProtocolHandler#0", "htpthand", new ChatServerHttpHandler());
+            ctx.fireChannelRead(msg);
+            //System.out.println(req.headers());
+            return;
         }
         else {
             System.out.println("ProtocolHandler] packet");
