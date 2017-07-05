fa394ce5e72b2bb52e77dd177d88a14ecac5875b
joey <ih8u2o@gmail.com>
2017-07-04 19:20:12 -0700

    Gave up on Netty HTTP Decoder; custom POST JSON decoder
    Netty's HttpRequestDecoder couldn't get the payload, so I've resorted
    to writing my own code that deserializes the last line of an
    HTTP Request which ((should)) contain the payload information
    as a JSON.
    
    Resorted to using a default char size for the char array as I parse
    through the last characters of the payload, but I will probably change
    this to String later on because parsing the Json as a char[]
    is not supported by Gson as far as I know.

--- src/com/test/chatserver/ChatServerProtocolHandler.java
+++ src/com/test/chatserver/ChatServerProtocolHandler.java
@@ -1,9 +1,13 @@
 package com.test.chatserver;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
@@ -12,8 +16,10 @@ import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.FullHttpMessage;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpContentDecoder;
 import io.netty.handler.codec.http.HttpMessage;
 import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpObjectDecoder;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpRequestDecoder;
@@ -21,10 +27,11 @@ import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
 
 public class ChatServerProtocolHandler extends HttpRequestDecoder {
     private ByteBuf buf;
-    private Stack<Object> decoded = new Stack<Object>();
+    private List<Object> decoded = new Stack<Object>();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -34,29 +41,60 @@ public class ChatServerProtocolHandler extends HttpRequestDecoder {
         if (buf.readableBytes() < 5) {
             return;
         }
-
-        System.out.println(buf.getCharSequence(0, buf.readableBytes(), Charset.defaultCharset()));
+        
         final int magic1 = buf.getUnsignedByte(buf.readerIndex());
         final int magic2 = buf.getUnsignedByte(buf.readerIndex() + 1);
         
-        if (isHttp(magic1,magic2)){
-            super.decode(ctx, buf, decoded);
-            System.out.println("[ProtocolHandler] got http request");
-            while (!decoded.isEmpty()){
-                
-                if (decoded.peek() instanceof LastHttpContent) {
-                    Object content = (Object) decoded.pop();
-                    System.out.println("last: " + content);
-                    continue;
-                }
-                
-                Object obj = (Object) decoded.pop();
-
-                System.out.println(obj.toString());
-                HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, 
-                        HttpResponseStatus.ACCEPTED);
-                ctx.writeAndFlush(response);
+        if (isPost(magic1,magic2)){
+            //Find the JSON object, starting from the end of the HTTP message
+            //Name + password should not be longer than 128 chars, so adding in the JSON
+            //formatting should not change that either
+            char[] chars = new char[128];
+            int val = 0;
+            for (int i = 0; i < chars.length; i++) {
+                val = buf.getUnsignedByte(buf.readableBytes() - chars.length + i);
+                chars[i] = (char) val;
             }
+            
+            boolean jsonComplete = false;
+            int startJson, endJson, currentIndex;
+            
+            startJson = endJson = 0;
+            currentIndex = chars.length - 1;
+            
+            //Find the JSON object
+            while (!jsonComplete) {
+                
+                if (chars[currentIndex] == '}') {
+                    endJson = currentIndex;
+                }
+                if (chars[currentIndex] == '{') {
+                    startJson = currentIndex;
+                    jsonComplete = true;
+                }
+                currentIndex--;
+                
+            }
+            
+            //make sure it's valid
+            if (startJson > endJson || endJson - startJson > 100) {
+                ctx.close();
+                return;
+            }
+            
+            char[] credentials = new char[endJson - startJson + 1];
+            
+            for (int i = 0; i < credentials.length; i++) {
+                credentials[i] = chars[startJson + i];
+            }
+            
+            String json = new String(credentials);
+            System.out.println(json);
+            //try to parse username and password
+            GsonBuilder builder = new GsonBuilder();
+            Object o = builder.create().fromJson(json, Object.class);
+            
+            System.out.println(o);
             
             return;
         } else {
@@ -67,17 +105,12 @@ public class ChatServerProtocolHandler extends HttpRequestDecoder {
         return;
     }
 
-    private static boolean isHttp(int magic1, int magic2) {
-        return  magic1 == 'G' && magic2 == 'E' || // GET
-                magic1 == 'P' && magic2 == 'O' || // POST
-                magic1 == 'P' && magic2 == 'U' || // PUT
-                magic1 == 'H' && magic2 == 'E' || // HEAD
-                magic1 == 'O' && magic2 == 'P' || // OPTIONS
-                magic1 == 'P' && magic2 == 'A' || // PATCH
-                magic1 == 'D' && magic2 == 'E' || // DELETE
-                magic1 == 'T' && magic2 == 'R' || // TRACE
-                magic1 == 'C' && magic2 == 'O'; // CONNECT
+    private static boolean isPost(int magic1, int magic2) {
+        return  magic1 == 'P' && magic2 == 'O';
     }
+
+
+
 
 
 }
