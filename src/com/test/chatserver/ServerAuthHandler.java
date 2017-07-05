5f349280c7a8284fdbbe0ab7eaae0d96e6dd5789
joey <ih8u2o@gmail.com>
2017-07-05 10:37:04 -0700

    Documentation for ServerAuthHandler; cleaned up channelRead

--- src/com/test/chatserver/ServerAuthHandler.java
+++ src/com/test/chatserver/ServerAuthHandler.java
@@ -25,45 +25,48 @@ import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 /**
- * Class used to authenticate users. Current implementation should handle 
+ * Class used to authenticate users. Current implementation should handle
  * FlatBuffers Serialized credentials as ByteBufs. Each serialized Credentials
- * should be prefixed by a 4 byte integer indicating the size in bytes of
- * the serialized FlatBuffer OR a JSON object conforming to the following schema:
+ * should be prefixed by a 4 byte integer indicating the size in bytes of the
+ * serialized FlatBuffer OR a JSON object conforming to the following schema:
  * {username: someUser, password: somePass}
  * 
- * Upon successful registration or authentication of the provided username/password
- * pair, ServerAuthHandler removes itself from the pipeline with handlers that 
- * are useful for server functionality.
+ * Upon successful registration or authentication of the provided
+ * username/password pair, ServerAuthHandler removes itself from the pipeline
+ * with handlers that are useful for server functionality.
  * 
  * @author jalbatross (Joey Albano)
  *
  */
 
 public class ServerAuthHandler extends ChannelInboundHandlerAdapter {
-    
-    private String username = new String();
+
     private ChannelGroup allUsers;
     private ArrayList<ChannelGroup> lobbies = new ArrayList<ChannelGroup>();
     private LoginAuthorizer login = new LoginAuthorizer();
 
+    //User credentials
+    private String username, pwdStr;
+    private char[] pwdChar;
+
     private Channel ch;
-    
+
     private static int MAX_MSG_LEN = FlatBuffersCodec.SERIALIZED_CRED_LEN;
-    
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
         ch = ctx.channel();
     }
-    
+
     public ServerAuthHandler(ChannelGroup grp, ArrayList<ChannelGroup> lobbies) {
         allUsers = grp;
         this.lobbies = lobbies;
     }
-    
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("[ServerAuthHandler] Auth Handler Called");
-        
+
         if (msg instanceof ByteBuf) {
             ByteBuf buf = (ByteBuf) msg;
 
@@ -73,23 +76,19 @@ public class ServerAuthHandler extends ChannelInboundHandlerAdapter {
                 return;
             }
 
-            Message received = Message.getRootAsMessage(buf.nioBuffer());
-
-            if (received.dataType() != Schema.Type.Credentials) {
-                System.out.println("ServerAuthHandler didn't receive Credentials!");
+            // Validate and get credentials
+            if (!validateFlatbuffersCredentials(buf)) {
                 ctx.close();
+                Arrays.fill(pwdChar, '0');
+                ((ByteBuf) msg).clear();
                 return;
             }
-            // Convert credential
-            Credentials credentials = FlatBuffersCodec.byteBufToData(buf.nioBuffer(), Credentials.class);
 
-            String user = credentials.username();
-            char[] pass = credentials.password().toCharArray();
-
-            if (login.verifyUser(user, pass)) {
+            // Check DB for credentials
+            if (login.verifyUser(username, pwdChar)) {
+                
+                //Send authorized packet to client
                 ByteBuffer auth = FlatBuffersCodec.authToByteBuffer(true);
-
-                System.out.println("auth size: " + auth.remaining());
 
                 ByteBuffer temp = ByteBuffer.allocate(4);
                 temp.putInt(auth.remaining());
@@ -111,78 +110,116 @@ public class ServerAuthHandler extends ChannelInboundHandlerAdapter {
                 System.out.println("Wrong user and pass");
 
             }
-            Arrays.fill(pass, '0');
+
+            // Clear sensitive information
+            Arrays.fill(pwdChar, '0');
+            ((ByteBuf) msg).clear();
+
         }
 
         else if (msg instanceof HttpContent) {
             HttpContent httpCred = (HttpContent) msg;
-            String jsonContent = httpCred.content().toString(StandardCharsets.UTF_8);
-            JsonObject obj = new JsonObject();
-            try {
-                obj = new JsonParser().parse(jsonContent).getAsJsonObject();
-                
-            } catch (Exception e) {
-                e.printStackTrace();
+            
+            if (!validateHttpCredentials(httpCred)) {
                 ctx.close();
-                System.out.println("failed to parse JSON during auth!");
-                System.out.println("Check credentials: bad formatting OR invalid creds");
+                return;
             }
-            
-            String username = obj.get("username").getAsString();
-            String pass = obj.get("password").getAsString();
-            
-            if (login.verifyUser(username, pass)) {
+
+            if (login.verifyUser(username, pwdStr)) {
                 System.out.println("[AuthHandler] Got correct user/pass (HTTP)");
-            }
+            } 
             else {
                 System.out.println("[AuthHandler] Got wrong user/pass (HTTP)");
             }
-            
-            return;
+
+            httpCred.release();
         }
 
         return;
 
-        /*
-        //TODO: Add stricter username requirements
-        if (credential.length() > 12 || credential.isEmpty()) {
-            System.out.println("received bad credential");
-            String errorMsg = "Username was longer than 12 chars or empty.";
-            ctx.writeAndFlush( new TextWebSocketFrame(errorMsg));
-            return;
-        }
-        
-        //TODO: Instead of names.constains(credential), query database for
-        // USERNAME and PASSWORD that client will submit here
-        if(allUsers.contains(ctx.channel()) || names.contains(credential)) {
-            System.out.println("either user already connected or duplicate username");
-            ctx.writeAndFlush(new TextWebSocketFrame("That username is being used!"));
-            return;
-        }
-        
-        //TODO: Change names.add(credential) to database query I think
-        //TODO: Send user list of lobbies and have them choose one after
-        //      succsessful login
-        if (allUsers.add(ctx.channel()) && names.add(credential)) {
-            System.out.println("added user w/ name: " + credential);
-            ctx.writeAndFlush(new TextWebSocketFrame("Login Successful! There are "
-                    + allUsers.size() + " connected users."));
-            ctx.pipeline().remove(this);
-            
-            //TODO: Make user choose lobby here or create new pipeline handler
-            // for lobby selector
-            //ctx.pipeline().addLast(new ChatServerLobbySelector());
-            ctx.pipeline().addLast(new ChatServerDecoder());
-            ctx.pipeline().addLast(new ChatServerHandler(allUsers, credential));
-        }
-        else {
-            System.out.println("[ServerAuthHandler] ERROR while adding valid credential");
-            ctx.writeAndFlush(new TextWebSocketFrame("Something weird happened! Sorry."));
-            names.remove(credential);
-            ctx.close();
-        }
-        */
-        
     }
-    
+
+    /**
+     * Private helper method to validate received credentials from a client 
+     * that sends credentials via HTTP POST.
+     * 
+     * Returns false if the JsonParser fails to correctly parse the JSON object
+     * OR if we fail to extract a username and password from expected JSON object
+     * fields after parsing.
+     * 
+     * A valid JSON credential looks like: 
+     * {username: someUser, password: somePass}
+     * 
+     * If valid credentials are received, then the username and pwdStr fields of
+     * the class are set to the received credentials.
+     * 
+     * @param httpCred   JSON credentials as HttpContent
+     * @return           True if credentials are correctly formatted, 
+     *                   false otherwise. Sets this username and 
+     *                   this pwdStr to received credentials.
+     */
+    private boolean validateHttpCredentials(HttpContent httpCred) {
+        String jsonContent = httpCred.content().toString(StandardCharsets.UTF_8);
+        JsonObject obj = new JsonObject();
+        try {
+            obj = new JsonParser().parse(jsonContent).getAsJsonObject();
+            this.username = obj.get("username").getAsString();
+            this.pwdStr = obj.get("password").getAsString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("failed to parse JSON during auth!");
+            System.out.println("Check credentials: bad formatting OR invalid creds");
+            return false;
+        }
+
+        return true;  
+    }
+
+    /**
+     * Private helper method that validates received ByteBuf credentials.
+     * 
+     * Returns false when the ByteBuf of the data does not have a valid schema
+     * type or if the FlatBuffersCodec fails to parse Credentials from
+     * buf.
+     * 
+     * If the function returns false, it will clear the pwdChar array of this
+     * by filling it with '0' for security.
+     * 
+     * Returns true otherwise, setting this.username and this.pwdChar to 
+     * the Credentials parsed from buf.
+     * 
+     * @param buf     A ByteBuf containing a serialized FlatBuffers Credentials
+     * @return        True if buf contains valid FlatBuffers Credentials. Sets
+     *                this.username and this.pwdChar to those credentials.
+     *                
+     *                False if we fail to parse Credentials from buf, then
+     *                clears pwdChar.
+     *                
+     * @see           Schema.Credentials
+     */
+    private boolean validateFlatbuffersCredentials(ByteBuf buf) {
+        Message received = Message.getRootAsMessage(buf.nioBuffer());
+
+        if (received.dataType() != Schema.Type.Credentials) {
+            System.out.println("ServerAuthHandler didn't receive Credentials!");
+            return false;
+        }
+
+        // Convert credential
+        try {
+        Credentials credentials = FlatBuffersCodec.byteBufToData(buf.nioBuffer(), Credentials.class);
+        this.username = credentials.username();
+        this.pwdChar = credentials.password().toCharArray();
+        }
+        catch (Exception e) {
+            System.out.println("[ServerAuthHandler] Couldn't get credentials from FlatBuffers!");
+            e.printStackTrace();
+            Arrays.fill(this.pwdChar, '0');
+            return false;
+        }
+
+        return true;
+    }
+
 }
