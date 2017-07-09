2bc55cdda38989a0f88566a69f6def536124d5d6
joey <ih8u2o@gmail.com>
2017-07-08 21:31:35 -0700

    Basic ping checker/disconnecter for browser

--- src/web/js/controllers/statusController.js
+++ src/web/js/controllers/statusController.js
@@ -1,20 +1,23 @@
 //statusController.js
 
 angular.module("pingApp").controller("statusController", function ($scope, $interval) {
-    $scope.toggle = false;
+    $scope.connected = false;
     $scope.action = "Connect";
 
     
     $scope.pingStatus = "off";
 
+
     var sendPings = false;
     var promise;
+    var lastPing;
     $scope.pinger = function() {
         sendPings = !sendPings;
 
         if (sendPings) {
             promise = $interval(function() {
                 console.log('sending ping at time ', Date.now());
+                lastPing = Date.now();
             }, 2000);
         }
         else {
@@ -23,5 +26,39 @@ angular.module("pingApp").controller("statusController", function ($scope, $inte
         }
         $scope.pingStatus = sendPings ? "on" : "off";
 
+    }
+
+    var checkerPromise;
+    var started = false;
+
+    var pingCheck = function() {
+        if (Date.now() - lastPing < 5000) {
+            console.log('maintain');
+            return 'connected';
+        }
+        else {
+            $scope.toggle = false;
+            console.log('lost connxn');
+            $interval.cancel(checkerPromise);
+            $scope.connected = false;
+            $scope.action = 'Disconnected';
+            return 'disconnected';
+
+        }
+    }
+
+    $scope.connect = function () {
+        $scope.connected = !$scope.connected;
+        $scope.action = $scope.connected ? 'Connected' : 'Disconnected';
+
+        if($scope.connected) {
+            checkerPromise = $interval(pingCheck, 1000);
+            console.log('applied checker');
+
+        }
+        else {
+            $interval.cancel(checkerPromise);
+            console.log('stopped checking');
+        }
     }
 });
