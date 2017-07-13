ca428bf933c2509fc354f29d505b38810db5cc52
joey <ih8u2o@gmail.com>
2017-07-12 19:51:02 -0700

    Fixed browser WS close delay bug
    Hacked together a solution by changing the UI to display that
    the connection was closed before performing cleanup for closing
    the websocket and removing the interval functions (pinger,
    statusChecker).
    
    This is because there is a delay between the time the WebSocket
    is closed and the time the WebSocket onClose function is actually
    executed for whatever reason. On the bright side, the server knows
    that the connection is closed and the status of the ws client side
    is closed as soon as ws.close() is called.
    
    Also put cleanup code into a function called closeWs().
    
    Documentation to follow.

--- src/web/js/controllers/statusController.js
+++ src/web/js/controllers/statusController.js
@@ -1,9 +1,10 @@
 //statusController.js
 
-angular.module("pingApp").controller("statusController", function ($scope, $interval) {
+angular.module("pingApp").controller("statusController", function($scope, $interval) {
     $scope.action = "Connect";
-    
+
     var connected = false;
+    var reconnecting = false;
 
     var ws;
 
@@ -11,17 +12,14 @@ angular.module("pingApp").controller("statusController", function ($scope, $inte
     var checkerPromise;
     var pingerPromise;
 
-    var reconnecting = false;
-
     //a long corresponding to Unix time in ms
     var lastPing;
-    
+
     var pingCheck = function(socket) {
         //good connection
         if (Date.now() - lastPing < 5000) {
             console.log('maintain');
-            
-            
+
             if (reconnecting) {
                 $interval.cancel(rcPromise);
                 reconnecting = false;
@@ -29,19 +27,22 @@ angular.module("pingApp").controller("statusController", function ($scope, $inte
 
             $scope.action = 'Connected';
         }
+
         //bad connection - spam pongs to network
-        else if (Date.now() - lastPing < 10000){
+        else if (Date.now() - lastPing < 10000) {
             console.log('Bad connection or maybe lost connection');
-            rcPromise = $interval(pinger,1000);
-            $scope.action = 'Reconnecting';
-            reconnecting = true;
+            if (!reconnecting) {
+                rcPromise = $interval(pinger, 1000);
+                $scope.action = 'Reconnecting';
+                reconnecting = true;
+            }
         }
 
         //lost connection, DC
         else {
-            socket.close();
+            closeWs();
             console.log('lost connxn');
-            
+
             if (reconnecting) {
                 $interval.cancel(rcPromise);
                 reconnecting = false;
@@ -55,49 +56,44 @@ angular.module("pingApp").controller("statusController", function ($scope, $inte
         socket.send('pong');
     }
 
-    $scope.connect = function () {
+    var closeWs = function() {
+        $scope.action = 'Disconnected';
+        connected = false;
+        ws.close();
+        $interval.cancel(checkerPromise);
+        $interval.cancel(pingerPromise);
+    }
+
+    $scope.connect = function() {
         if (!connected) {
-          ws = new WebSocket("ws://localhost:8080/websocket");
+            ws = new WebSocket("ws://localhost:8080/websocket");
 
-          ws.onopen = function (event) {
-              console.log('successfully connected');
-              lastPing = Date.now();
-              pingerPromise = $interval(function() {pinger(ws)},3000);
-              checkerPromise = $interval(function() {pingCheck(ws)}, 1000);
-              connected = true;
+            ws.onopen = function(event) {
+                console.log('successfully connected');
+                lastPing = Date.now();
+                pingerPromise = $interval(function() {
+                    pinger(ws)
+                }, 3000);
+                checkerPromise = $interval(function() {
+                    pingCheck(ws)
+                }, 1000);
+                connected = true;
 
-              $scope.action = 'Connected';
+                $scope.action = 'Connected';
 
-          }
+            }
 
-          ws.onmessage = function(event) {
-              console.log('received msg from server');
-              lastPing = Date.now();
-          }
+            ws.onmessage = function(event) {
+                console.log('received msg from server');
+                lastPing = Date.now();
+            }
 
-          ws.onclose = function(event) {
-              console.log('disconnected from server');
-              if ($interval.cancel(checkerPromise)) {
-                console.log('turned off checker');
-              }
-              if ($interval.cancel(pingerPromise)) {
-                console.log('turned off pinger');
-              }
-              checkerPromise = undefined;
-              pingerPromise = undefined;
-              connected = false;
-
-              $scope.action = 'Disconnected';
-              $scope.$apply();
-
-              return;
-          }
-        }
-
+            ws.onclose = function(event) {
+                console.log('ws socket closed');
+            }
+        } 
         else {
-            $interval.cancel(checkerPromise);
-            $interval.cancel(pingerPromise);
-            ws.close();
+            closeWs();
             console.log('closed ws connection');
         }
     }
